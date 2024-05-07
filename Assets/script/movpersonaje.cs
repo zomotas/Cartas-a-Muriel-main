895e96af60a147b759c9253a86c4aa3419f26d0d
yeguagit <jesusrt133@gmail.com>
2024-05-07 13:14:47 +0200

    balanciin

---
+++ Assets/script/movpersonaje.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movpersonaje : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //hola quiero hacer un commit 
+    void Start()
+    {
+        Debug.Log ("hola");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
